6700da329c4ea0dd354b62b45d26430376a6191a
Elizaveta Kashilova <36787577+golonr1na@users.noreply.github.com>
2024-02-27 17:58:31 +0500

    Update lab8.py

--- codes/lab8.py
+++ codes/lab8.py
@@ -3,9 +3,6 @@ while num < 10:
     num += 3
     if num % 2 == 0:
         print(f'Число {num} четное')
-    elif num > 10:
-      break
     else:
         print(f'Число {num} нечетное')
         num -= 1
-
